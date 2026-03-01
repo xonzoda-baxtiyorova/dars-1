4d1205b5fc11f639982dd92c5f4262aa9f24a7fe
Abdulaziz <sadullayevabdulaziz2009@gmail.com>
2026-03-01 16:09:38 +0500

    added dars 1

---
+++ app.js
@@ -0,0 +1,5 @@
+typeof 0              // Returns "number"
+typeof 314            // Returns "number"
+typeof 3.14           // Returns "number"
+typeof (3)            // Returns "number"
+typeof (3 + 4)        // Returns "number"
